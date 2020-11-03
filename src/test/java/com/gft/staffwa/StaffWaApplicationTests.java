3d1d26c497dcdb0347bf97ce6e169f0a1ee1a92c
lspt <lucas.pedroso@gft.com>
2020-11-03 14:53:55 -0300

    Initial commit

---
+++ src/test/java/com/gft/staffwa/StaffWaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.staffwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffWaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
