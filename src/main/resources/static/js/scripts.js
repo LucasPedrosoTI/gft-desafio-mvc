d5a21a9f14489bd76420c99cba5def0c92473d7c
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-11-13 15:22:08 -0300

    implementada feature para mostrar mais itens por página

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -23,3 +23,27 @@ $("#confirmacaoExclusaoModal").on("show.bs.modal", function (event) {
 $(function () {
   $('[rel="tooltip"]').tooltip();
 });
+
+$("#pageSize").change(function () {
+  let url = window.location.toString();
+
+  if (url.includes("size")) {
+    var indexSize = url.indexOf("size");
+    var sizeSubstr = url.slice(indexSize - 1);
+    url = url.replace(sizeSubstr, "");
+  }
+
+  if (url.includes("pageSize")) {
+    var indexSize = url.indexOf("pageSize");
+    var sizeSubstr = url.slice(indexSize - 1);
+    url = url.replace(sizeSubstr, "");
+  }
+
+  if (url.includes("?")) {
+    url = `${url}&size=${this.value}`;
+  } else {
+    url = `${url}?size=${this.value}`;
+  }
+
+  window.open(url, "_self");
+});
