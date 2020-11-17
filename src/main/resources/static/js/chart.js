e82f79cdf7a014335717fe73fe257ed6d9549fc6
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-11-17 17:52:46 -0300

    chart fix e criação do properties para produção

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -8,68 +8,70 @@ function normalizarDatas(array) {
 }
 
 async function drawChart() {
-  const baseUrl = "http://localhost:8080/wa";
+  const baseUrl = window.location.origin;
 
   // Define the chart to be drawn.
 
-  const response = await fetch(`${baseUrl}/funcionarios/termino-wa`);
+  const response = await fetch(`${baseUrl}/wa/funcionarios/termino-wa`);
   const funcionarios = await response.json();
 
-  if (funcionarios.length < 2) {
-    return;
+  if (funcionarios.length > 1) {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn("date", "Data");
+    data.addColumn("number", "Quantidade Funcionários");
+    data.addRows(normalizarDatas(funcionarios));
+
+    const options = {
+      title: "Próximos Vencimentos de WA",
+      legend: { position: "bottom" },
+      vAxis: {
+        format: "#",
+        minValue: 0,
+      },
+      hAxis: {
+        format: "dd/MM/yyyy",
+        ticks: data.getDistinctValues(0),
+        gridlines: {
+          count: -1,
+        },
+      },
+    };
+
+    // Instantiate and draw the chart.
+    const chart = new google.visualization.LineChart(
+      document.getElementById("funcionarios-termino-wa")
+    );
+
+    chart.draw(data, options);
   }
 
-  const responseVagas = await fetch(`${baseUrl}/vagas/por-data`);
+  const responseVagas = await fetch(`${baseUrl}/wa/vagas/por-data`);
   const vagas = await responseVagas.json();
 
-  const data = new google.visualization.DataTable();
-  const data2 = new google.visualization.DataTable();
+  if (vagas.length > 1) {
+    const data2 = new google.visualization.DataTable();
 
-  data.addColumn("date", "Data");
-  data.addColumn("number", "Quantidade Funcionários");
-  data.addRows(normalizarDatas(funcionarios));
+    data2.addColumn("date", "Data");
+    data2.addColumn("number", "Quantidade Vagas");
+    data2.addRows(normalizarDatas(vagas));
 
-  data2.addColumn("date", "Data");
-  data2.addColumn("number", "Quantidade Vagas");
-  data2.addRows(normalizarDatas(vagas));
-
-  const options = {
-    title: "Próximos Vencimentos de WA",
-    legend: { position: "bottom" },
-    vAxis: {
-      format: "#",
-      minValue: 0,
-    },
-    hAxis: {
-      format: "dd/MM/yyyy",
-      ticks: data.getDistinctValues(0),
-      gridlines: {
-        count: -1,
+    const options2 = {
+      title: "Vagas Lançadas por Data",
+      legend: { position: "bottom" },
+      vAxis: {
+        format: "#",
+        minValue: 0,
       },
-    },
-  };
+      hAxis: {
+        format: "dd/MM/yyyy",
+      },
+    };
 
-  const options2 = {
-    title: "Vagas Lançadas por Data",
-    legend: { position: "bottom" },
-    vAxis: {
-      format: "#",
-      minValue: 0,
-    },
-    hAxis: {
-      format: "dd/MM/yyyy",
-    },
-  };
+    const chart2 = new google.visualization.LineChart(
+      document.getElementById("vagas")
+    );
 
-  // Instantiate and draw the chart.
-  const chart = new google.visualization.LineChart(
-    document.getElementById("funcionarios-termino-wa")
-  );
-
-  const chart2 = new google.visualization.LineChart(
-    document.getElementById("vagas")
-  );
-
-  chart.draw(data, options);
-  chart2.draw(data2, options2);
+    chart2.draw(data2, options2);
+  }
 }
